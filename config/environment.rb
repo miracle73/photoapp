db20aee2535562d670bcfca8113fcaef62e53c86
miracle73 <nwadiaromiraclechukwuma@gmail.com>
2020-06-25 06:29:05 +0100

    Email must work

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+#"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"
 
 ActionMailer::Base.smtp_settings = {
 :address => 'smtp.sendgrid.net',
 :port => '587',
 :authentication => :plain,
-:user_name => ENV['apikey'],
-:password => ENV['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'],
+:user_name => ENV['SENDGRID_USERNAME'],
+:password => ENV['SENDGRID_PASSWORD'],
 :domain => 'heroku.com',
 :enable_starttls_auto => true
 }
